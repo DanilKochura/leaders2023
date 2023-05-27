cc564cb6e7f7e92c7a4f0d5e277db520e70c3179
DanilKochura <kochura017@yandex.ru>
2023-05-27 20:36:32 +0300

    Работа с API

--- web/controller/api/controller.php
+++ web/controller/api/controller.php
@@ -1,37 +1,86 @@
 <?php
+
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
 require_once ROOT.'model/DB.php';
 
 
 global $route;
 
+$db = new DB();
 
 
 if($route[1] == 'demand')
 {
-
     $resp = [];
-    $db = new DB();
+
     $res = $db->conn->query("SELECT * from class where flt_num = '{$_POST['flight']}' and `dd` = '{$_POST['date']}' AND `sorg` = '{$_POST['from']}' AND `sdst` = '{$_POST['to']}' and `seg_class_code` = '{$_POST['class']}'");
     if($res->num_rows == 0)
     {
         die(json_encode([]));
     }
+
     $i = 0;
     while($row = $res->fetch_assoc())
     {
         $resp['count'] = $res->num_rows;
         $resp['dates'][$i] = $row['sdat_s'];
         $resp['pass'][$i] = $row['pass_bk'];
-        if(isset($resp['pass'][$i-1]))
+        $resp['pass_sqrt'][$i] = $row['pass_bk'];
+        $resp['pass_div'][$i] = $row['pass_bk'];
+        $resp['pass_def'][$i] = $row['pass_bk'];
+        if(isset($resp['pass_sqrt'][$i-1]))
         {
-            $resp['pass'][$i-1] -= $row['pass_bk'];
+            $resp['pass_sqrt'][$i-1] = sqrt(pow($row['pass_sqrt'] - $resp['pass_sqrt'][$i-1], 2)/2);
+        }
+        if(isset($resp['pass_def'][$i-1]))
+        {
+            $resp['pass_def'][$i-1] = $resp['pass_def'][$i-1]-$resp['pass_def'][$i];
+        }
+        if(isset($resp['pass_div'][$i-1]))
+        {
+            $resp['pass_div'][$i-1] = $resp['pass_div'][$i]/$resp['pass_div'][$i-1];
         }
         $i++;
+
         $resp['label'] = $row['seg_class_code'];
     }
     echo json_encode($resp);
-
-} elseif($route[2] == 'flights')
+} elseif($route[1] == 'seasons')
 {
+    $resp = [];
 
+    $res = $db->conn->query("SELECT * from class where flt_num = '{$_POST['flight']}' and `sorg` = '{$_POST['from']}' AND `sdst` = '{$_POST['to']}' and `seg_class_code` = '{$_POST['class']}' and year(dd) = '{$_POST['year']}' and dtd = -1");
+    if($res->num_rows == 0)
+    {
+        die(json_encode([]));
+    }
+
+    $i = 0;
+    while($row = $res->fetch_assoc())
+    {
+        $resp['count'] = $res->num_rows;
+        $resp['dates'][$i] = $row['sdat_s'];
+        $resp['pass'][$i] = $row['pass_bk'];
+        $i++;
+
+        $resp['label'] = $row['seg_class_code'];
+    }
+    echo json_encode($resp);
+} elseif($route[1] == 'search_flight')
+{
+    $resp = [];
+
+    $res = $db->conn->query("SELECT distinct flt_num from class where sorg = '{$_POST['from']}' and sdst = '{$_POST['to']}'");
+    if($res->num_rows == 0)
+    {
+        die(json_encode([]));
+    }
+
+    $i = 0;
+    while($row = $res->fetch_assoc())
+    {
+        $resp[] = $row['flt_num'];
+    }
+    echo json_encode($resp);
 }
--- web/controller/controller.php
+++ web/controller/controller.php
@@ -1,4 +1,7 @@
 <?php
+
+ini_set('display_errors', 1);
+error_reporting(E_ERROR);
 global $route;
 require_once ROOT.'/model/DB.php';
 require_once ROOT.'/views/inc/header.php';
@@ -8,41 +11,18 @@ if($route[0] == '/')
 {
 
     require_once ROOT.'/views/index.php';
-    require_once ROOT.'/views/inc/footer.php';
-} else
+} elseif ($route[0] == 'dynamic')
 {
-    $_POST =
-        [
-          'flight' => 1130,
-          'from' => 'SVO',
-          'to' => 'AER',
-          'date' => '2018-09-03'
-        ];
-    $resp = [];
-    $db = new DB();
-    $query = "SELECT * from class where flt_num = '{$_POST['flight']}' and `dd` = '{$_POST['date']}' AND `sorg` = '{$_POST['from']}' AND `sdst` = '{$_POST['to']}'";
-    if($_POST['class'])
-    {
-        $query.="and `seg_class_code` = '{$_POST['class']}'";
-    }
-    $res = $db->conn->query($query);
-    if($res->num_rows == 0)
-    {
-        die(json_encode([]));
-    }
-    $i = 0;
-    while($row = $res->fetch_assoc())
-    {
-        $resp['count'] = $res->num_rows;
-        $resp['data'][$row['seg_class_code']]['dates'][$i] = $row['sdat_s'];
-        $resp['data'][$row['seg_class_code']]['pass'][$i] = $row['pass_bk'];
-        if(isset($resp['pass'][$i-1]))
-        {
-            $resp['data'][$row['seg_class_code']]['pass'][$i-1] -= $row['pass_bk'];
-        }
-        $i++;
-    }
-    echo json_encode($resp);
-    die();
+    require_once ROOT.'/views/dynamic.php';
+}elseif ($route[0] == 'seasons')
+{
+    require_once ROOT.'/views/seasons.php';
+}elseif ($route[0] == 'profiles')
+{
+    require_once ROOT.'/views/profiles.php';
+}elseif ($route[0] == 'predicts')
+{
+    require_once ROOT.'/views/predicts.php';
 }
+require_once ROOT.'/views/inc/footer.php';
 
